169f789a0d098f94ecb645cdc2cf273ea9a15b00
Ubuntu <hester@hester.ahv55eo0d0uudejsmiqcgegsfa.bx.internal.cloudapp.net>
2021-06-15 10:45:35 +0000

    Database linked

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
  
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
- 
 db = SQLAlchemy(app)
  
 class Todo(db.Model):
@@ -13,8 +12,11 @@ class Todo(db.Model):
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
- 
- 
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
