1c4b27ae9ec503dffe7c967c146430e7b53b2cb5
Ubuntu <hester@hester.ahv55eo0d0uudejsmiqcgegsfa.bx.internal.cloudapp.net>
2021-06-15 11:22:06 +0000

    Jinja, Fresh DB and HTML variable pass

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
  
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db' 
@@ -16,15 +16,24 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    todo = Todo(title="First", desc="Test Commit")
+    db.session.add(todo)
+    db.session.commit()
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return render_template('index.html', allTodo=allTodo)
  
- 
-@app.route('/products')
-def products():
-    return 'Hello, Products!'
+@app.route('/show')
+def show():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'Hello, ALl!'
+  
+# @app.route('/products')
+# def products():
+#     return 'Hello, Products!'
  
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
